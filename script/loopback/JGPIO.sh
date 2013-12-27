60d22e44044587ad1827856bb781dff9ac50bc71
planykao <planykao@gmail.com>
2013-12-27 18:13:51 +0800

    Add configuration file for S0101/S0381/S0571. Add JGPIO.sh for GOIO loopback test.

---
+++ script/loopback/JGPIO.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Version: 1.0
+# Build Date: 09-30-2013
+
+
+
+# Check JGPIO
+
+
+echo "*******************************************"
+echo "*     Please install Jumpers in JGPIO     *"
+echo "*******************************************"
+read -p "Press Enter Key to Continue if Jumpers is install... "
+
+./gpio-loopback $1
+
+if [ "$?" == "0" ]; then
+	echo -e "\033[0;30;42m ************************** \033[0m"
+	echo -e "\033[0;30;42m *     Test GPIO Pass     * \033[0m"
+	echo -e "\033[0;30;42m ************************** \033[0m"
+else
+	echo -e "\033[0;30;41m ************************** \033[0m"
+	echo -e "\033[0;30;41m *     Test GPIO Fail     * \033[0m"
+	echo -e "\033[0;30;41m ************************** \033[0m"
+	read -p "Press Enter Key to Continue... "
+    echo ""
+	exit 1
+fi
+
+exit 0
